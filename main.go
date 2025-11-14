9f0a2bf93359e8ebf81d87f68522631e9dcb13b7
Clown <308480661@qq.com>
2025-11-14 17:36:04 +0800

    跨域问题

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
-	"webook/internal/web"
-
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"strings"
+	"time"
+	"webook/internal/web"
 )
 
 func main() {
 	//server是一个web服务器的实例
 	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		//AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PATCH"},
+		AllowHeaders:     []string{"authorization", "Content-Type"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				return true
+			}
+			return strings.Contains(origin, "http://localhost")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	u := web.NewUserHandler()
 	u.RegisterRoutesV1(server.Group("/user"))
